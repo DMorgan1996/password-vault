1b3379ee0efcf505967f765aa02f2c2f54533470
DM-code-ui <dan96morgan@gmail.com>
2026-07-10 19:40:54 +0100

    Set up Spring Boot backend

---
+++ backend/passwordvault/src/test/java/com/daniel/passwordvault/PasswordvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.passwordvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
