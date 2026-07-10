bb2fb5151bc0209df3b97b7b410ea92a8ff9530a
DM-code-ui <dan96morgan@gmail.com>
2026-07-10 20:20:02 +0100

    Add credential getters and setters

--- backend/passwordvault/src/main/java/com/daniel/passwordvault/entity/Credential.java
+++ backend/passwordvault/src/main/java/com/daniel/passwordvault/entity/Credential.java
@@ -16,4 +16,44 @@ public class Credential {
     private String username;
     private String password;
     private String notes;
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getWebsite() {
+        return website;
+    }
+    
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getNotes() {
+        return notes;
+    }
+    
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
